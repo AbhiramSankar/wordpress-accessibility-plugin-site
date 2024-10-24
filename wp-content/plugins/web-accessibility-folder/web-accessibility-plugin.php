c77aff3de8eaed1e4b625d8935257e5c4d554391
Abhiram Sankar <abhiramsankar102000@gmail.com>
2024-10-23 20:35:09 -0400

    Wordpress and plugin setup

---
+++ wp-content/plugins/web-accessibility-folder/web-accessibility-plugin.php
@@ -0,0 +1,10 @@
+/*
+ * Plugin Name:       WP Web Accessibility Plugin
+ * Description:       This is a plugin for implementing the website accessibility features.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Abhiram Sankar & Arth Ashwinbhai Patel 
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ */
