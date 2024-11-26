764c13e87336ad42956e581e3a0cf16f9d49ed53
Abhiram Sankar <abhiramsankar102000@gmail.com>
2024-11-26 14:28:04 -0500

    session storage, tts improvement, admin implementation and  other fixes

--- wp-content/plugins/web-accessibility-plugin/assets/js/script.js
+++ wp-content/plugins/web-accessibility-plugin/assets/js/script.js
@@ -1,30 +1,114 @@
 const $ = jQuery;
 
-let globalVal = {
-  fontSize: 0,
-  colorAndContrast: {
-    filter: "none",
-    contrast: 1,
-  },
-  tts: false,
-  ttsSetting: {
-    voice: 0,
-    speed: 1,
-    pitch: 1,
-  },
-};
+let globalVal;
+if (!sessionStorage.getItem("globalVal")) {
+  sessionStorage.setItem(
+    "globalVal",
+    JSON.stringify({
+      fontSize: 0,
+      colorAndContrast: {
+        filter: "none",
+        contrast: 1,
+      },
+      tts: false,
+      ttsSetting: {
+        voice: 0,
+        speed: 1,
+        pitch: 1,
+      },
+    })
+  );
+}
+globalVal = JSON.parse(sessionStorage.getItem("globalVal"));
 
 $(() => {
-  //UI Controls
+  /* UI Controls */
   $("#accessibility-button").on("click", () => {
     // Toggle accessibility menu
     $("#accessibility-menu").slideToggle();
   });
 
-  //Update font size
-  $(".font-size-control > .control > button").on("click", function () {
-    let btn = this;
+  //Color Mode Select Init
+  $("#color-blind-filters").select2({
+    dropdownParent: $("#color-blind-wrapper"),
+    width: "100%",
+    minimumResultsForSearch: -1,
+  });
 
+  //TTS Voice Data Select Init
+  var voicesData = [];
+  window.speechSynthesis.onvoiceschanged = function () {
+    let voicesList = window.speechSynthesis.getVoices();
+    voicesList.forEach((voice, index) => {
+      voicesData.push({ id: index, text: voice.name });
+    });
+    $("#tss-voice").select2({
+      data: voicesData,
+      dropdownParent: $("#tts-voice-wrapper"),
+      width: "100%",
+      minimumResultsForSearch: -1,
+    });
+
+    //Set stored font size
+    if (globalVal.fontSize > 0) {
+      updateFontSize(function (element) {
+        // console.log($(".select2-selection__rendered"), $(".select2"), "CJ1");
+        var size = parseInt($(element).css("font-size"));
+        if (size > 0) {
+          size = size + globalVal.fontSize + "px";
+        }
+
+        if (!$(element).hasClass(".select2-selection__rendered")) {
+          $(element).css({
+            "font-size": size,
+          });
+        }
+      });
+      $(".font-size-control > .control > span").html(`${globalVal.fontSize}`);
+    }
+
+    //Set stored filter
+    if (globalVal.colorAndContrast.filter !== "none") {
+      $("#color-blind-filters")
+        .val(globalVal.colorAndContrast.filter)
+        .trigger("change");
+    }
+
+    //Set stored contrast
+    if (globalVal.colorAndContrast.contrast !== 1) {
+      $("input[name='color-mode']")
+        .val((globalVal.colorAndContrast.contrast - 1) * 100)
+        .trigger("change");
+    }
+
+    if (globalVal.tts) {
+      $("input[name='tts']").prop("checked", globalVal.tts).trigger("change");
+    }
+
+    if (globalVal.ttsSetting.voice !== 0) {
+      $("#tss-voice").val(globalVal.ttsSetting.voice).trigger("change");
+    }
+
+    if (globalVal.ttsSetting.speed !== 1) {
+      $("input[name='tss-option']#speed")
+        .val(globalVal.ttsSetting.speed)
+        .trigger("change");
+    }
+
+    if (globalVal.ttsSetting.pitch !== 1) {
+      $("input[name='tss-option']#pitch")
+        .val(globalVal.ttsSetting.pitch)
+        .trigger("change");
+    }
+  };
+
+  $("#tss-voice").on("select2:select", function () {
+    globalVal.ttsSetting.voice = this.value;
+    sessionStorage.setItem("globalVal", JSON.stringify(globalVal));
+  });
+
+  // Function to update font size
+  function updateFontSize(callback = null) {
     $("body *")
       .filter(function () {
         // Check if the element has at least one text node with non-whitespace content
@@ -40,20 +124,28 @@ $(() => {
         );
       })
       .each(function () {
-        var size = parseInt($(this).css("font-size"));
-        if (size > 0) {
-          if (btn.className == "plus") {
-            size = size + 1 + "px";
-          } else {
-            if (globalVal.fontSize > 0) {
-              size = size - 1 + "px";
-            }
+        callback(this);
+      });
+  }
+
+  //Update font size
+  $(".font-size-control > .control > button").on("click", function () {
+    let btn = this;
+    updateFontSize(function (element) {
+      var size = parseInt($(element).css("font-size"));
+      if (size > 0) {
+        if (btn.className == "plus") {
+          size = size + 1 + "px";
+        } else {
+          if (globalVal.fontSize > 0) {
+            size = size - 1 + "px";
           }
         }
-        $(this).css({
-          "font-size": size,
-        });
+      }
+      $(element).css({
+        "font-size": size,
       });
+    });
 
     if (this.className == "plus") {
       globalVal.fontSize += 1;
@@ -64,22 +156,18 @@ $(() => {
     }
 
     $(".font-size-control > .control > span").html(`${globalVal.fontSize}`);
+    sessionStorage.setItem("globalVal", JSON.stringify(globalVal));
   });
 
-  //Color Mode
-  $("#color-blind-filters").select2({
-    dropdownParent: $("#color-blind-wrapper"),
-    width: "100%",
-    minimumResultsForSearch: -1,
-  });
-
-  $("#color-blind-filters").on("select2:select", function () {
+  //Update Color Blind Filter
+  $("#color-blind-filters").on("change", function () {
     // console.log(this.value);
     globalVal.colorAndContrast.filter = this.value;
     $("html").css({
-      filter: `contrast(${globalVal.colorAndContrast.contrast}) url("wp-content/plugins/web-accessibility-plugin/assets/svg/colorBlindFilters.svg#${globalVal.colorAndContrast.filter}")`,
-      "webkit-filter": `contrast(${globalVal.colorAndContrast.contrast}) url("wp-content/plugins/web-accessibility-plugin/assets/svg/colorBlindFilters.svg#${globalVal.colorAndContrast.filter}")`,
+      filter: `contrast(${globalVal.colorAndContrast.contrast}) url("/wordpress-accessibility-plugin-site/wp-content/plugins/web-accessibility-plugin/assets/svg/colorBlindFilters.svg#${globalVal.colorAndContrast.filter}")`,
+      "webkit-filter": `contrast(${globalVal.colorAndContrast.contrast}) url("/wordpress-accessibility-plugin-site/wp-content/plugins/web-accessibility-plugin/assets/svg/colorBlindFilters.svg#${globalVal.colorAndContrast.filter}")`,
     });
+    sessionStorage.setItem("globalVal", JSON.stringify(globalVal));
   });
 
   $("input[name='color-mode']").on("change", function () {
@@ -92,9 +180,13 @@ $(() => {
     // }
     // console.log(this.id, this.value, globalVal);
     $("html").css({
-      filter: `contrast(${globalVal.colorAndContrast.contrast}) url("wp-content/plugins/web-accessibility-plugin/assets/svg/colorBlindFilters.svg#${globalVal.colorAndContrast.filter}")`,
-      "webkit-filter": `contrast(${globalVal.colorAndContrast.contrast}) url("wp-content/plugins/web-accessibility-plugin/assets/svg/colorBlindFilters.svg#${globalVal.colorAndContrast.filter}")`,
+      filter: `contrast(${globalVal.colorAndContrast.contrast}) url("/wordpress-accessibility-plugin-site/wp-content/plugins/web-accessibility-plugin/assets/svg/colorBlindFilters.svg#${globalVal.colorAndContrast.filter}")`,
+      "webkit-filter": `contrast(${globalVal.colorAndContrast.contrast}) url("/wordpress-accessibility-plugin-site/wp-content/plugins/web-accessibility-plugin/assets/svg/colorBlindFilters.svg#${globalVal.colorAndContrast.filter}")`,
     });
+    if (globalVal.tts) {
+      ttsEngine(`Contrast changed to ${globalVal.colorAndContrast.contrast}`);
+    }
+    sessionStorage.setItem("globalVal", JSON.stringify(globalVal));
   });
 
   //Reset Color Mode
@@ -103,7 +195,13 @@ $(() => {
     $("html").css({
       filter: "contrast(1)",
     });
-    $("input[name='color-mode']").val(0);
+    $("#color-blind-filters").val("none").trigger("change");
+    $("input[name='color-mode']").val(0).trigger("change");
+    if (globalVal.tts) {
+      ttsEngine(`Contrast reset to ${globalVal.colorAndContrast.contrast}`);
+    }
+    sessionStorage.setItem("globalVal", JSON.stringify(globalVal));
+    // $("input[name='color-mode']").val(0);
   });
 
   //TTS Setting
@@ -113,56 +211,142 @@ $(() => {
     if (element.is(":visible") && !element.is(":hidden")) {
       if (element.is("input") || element.is("textarea")) {
         // console.log(element);
-        $("body").keydown((e) => {
-          if (e.ctrlKey && e.altKey && e.key === "s" && element.is(":focus")) {
-            ttsEngine(element.val());
-          }
-        });
+        let type = element.attr("type");
+        let id = element.attr("id");
+        // console.log(type);
+        if (type == "checkbox" || type == "radio") {
+          let checked = element.is(":checked") ? "checked" : "unchecked";
+          ttsEngine((id ? id + " " : "") + type + " input");
+          element.on("click", () => {
+            ttsEngine(`${id ? id : type} is ${checked}`);
+          });
+        } else if (type == "slider") {
+          ttsEngine((id ? id + " " : "") + type + " input");
+          element.on("change", () => {
+            ttsEngine(`${id ? id : type} changed to ${element.val()}`);
+          });
+        } else if (type == "submit") {
+          ttsEngine(element.val());
+        } else {
+          ttsEngine(type + " input");
+          $("body").keydown((e) => {
+            if (
+              e.ctrlKey &&
+              e.altKey &&
+              e.key === "s" &&
+              element.is(":focus")
+            ) {
+              ttsEngine(codeParse(element.val()));
+            }
+          });
+        }
       } else if (element.is("p")) {
-        ttsEngine(element.text());
+        let text = "";
+        if (element.text()) {
+          text = text + element.text();
+        }
+        if (element.attr("aria-label")) {
+          text = text + element.attr("aria-label");
+        }
+        if (element.attr("aria-description")) {
+          text = text + element.attr("aria-description");
+        }
+        ttsEngine(codeParse(text));
       } else if (element.is("img") || element.is("video")) {
         ttsEngine(element.attr("alt"));
       } else {
         if (hasTextNodeWithContent(event.target)) {
-          ttsEngine(element.text());
+          let text = "";
+          if (element.text()) {
+            text = text + element.text();
+          }
+          if (element.attr("aria-label")) {
+            text = text + element.attr("aria-label");
+          }
+          if (element.attr("aria-description")) {
+            text = text + element.attr("aria-description");
+          }
+          ttsEngine(codeParse(text));
         }
       }
     }
   }
 
   function hasTextNodeWithContent(element) {
-    return Array.from(element.childNodes).some(
+    let hasTextNode = Array.from(element.childNodes).some(
       (node) =>
         node.nodeType === Node.TEXT_NODE && node.nodeValue.trim().length > 0
     );
+
+    let hasAria =
+      ($(element).attr("aria-label") &&
+        $.trim($(element).attr("aria-label")).length > 0) ||
+      ($(element).attr("aria-description") &&
+        $.trim($(element).attr("aria-description")).length > 0);
+
+    return hasTextNode || hasAria;
   }
 
-  var voicesData = [];
-  window.speechSynthesis.onvoiceschanged = function () {
-    let voicesList = window.speechSynthesis.getVoices();
-    voicesList.forEach((voice, index) => {
-      voicesData.push({ id: index, text: voice.name });
-    });
-    $("#tss-voice").select2({
-      data: voicesData,
-      dropdownParent: $("#tts-voice-wrapper"),
-      width: "100%",
-      minimumResultsForSearch: -1,
-    });
-  };
-  $("#tss-voice").on("select2:select", function () {
-    globalVal.ttsSetting.voice = this.value;
-  });
+  function codeParse(text) {
+    // text.replaceAll("!", "not").replaceAll("!==", "not equal to");
+    let readable;
+    console.log(detectContentType(text), text);
+    if (detectContentType(text) == "HTML") {
+      readable = text
+        .replace(/<!--(.*?)/g, "HTML comment: $1.")
+        .replace("-->", "HTML comment end.");
+      // console.log(readable);
+    } else {
+      readable = text
+        .replace(/\/\/(.*)/g, "Comment: $1.")
+        .replace(
+          /\/\*([\s\S]*?)/g,
+          (match, p1) => `Comment block: ${p1.trim().replace(/\n/g, ". ")}`
+        )
+        .replace(/\*\//g, (match, p1) => `Comment block end.`)
+        .replace(/=>/g, "Function")
+        .replace(/\{/g, " open curly brace ")
+        .replace(/\}/g, " close curly brace ")
+        .replace(">", "greater than")
+        .replace("<", "less than")
+        .replace("!==", "not equal to")
+        .replace(/!/g, "not");
+      console.log(readable, text);
+    }
+    return readable;
+  }
+
+  function detectContentType(text) {
+    const htmlRegex = /<\/?[a-z][^>]*>/i;
+    const commentRegex = /<!--[\s\S]*?-->/;
+    if (htmlRegex.test(text) || commentRegex.test(text)) {
+      return "HTML";
+    }
+
+    if (/^[\s\S]*{[\s\S]*:[\s\S]*;[\s\S]*}$/gm.test(text)) {
+      return "CSS";
+    }
+
+    return "Others";
+  }
 
   $("input[name='tss-option']").on("change", function () {
-    if(this.id == "speed"){
+    if (this.id == "speed") {
       globalVal.ttsSetting.speed = this.value;
+      if (globalVal.tts) {
+        ttsEngine(`Voice Speed changed to ${globalVal.ttsSetting.speed}`);
+      }
     }
-    if(this.id == "pitch"){
+    if (this.id == "pitch") {
       globalVal.ttsSetting.pitch = this.value;
+      if (globalVal.tts) {
+        ttsEngine(`Voice Pitch changed to ${globalVal.ttsSetting.pitch}`);
+      }
     }
-    console.log(globalVal.ttsSetting)
-  })
+
+    // console.log(globalVal.ttsSetting);
+    sessionStorage.setItem("globalVal", JSON.stringify(globalVal));
+  });
 
   function ttsEngine(text) {
     speechSynthesis.cancel();
@@ -180,19 +364,26 @@ $(() => {
     globalVal.ttsSetting.speed = 1;
     globalVal.ttsSetting.voice = 0;
     $("input[name='tss-option']").val(1);
-    $("#tss-voice").val(0)
-    $('#tss-voice').trigger('change');
+    $("#tss-voice").val(0);
+    $("#tss-voice").trigger("change");
+    sessionStorage.setItem("globalVal", JSON.stringify(globalVal));
+    if (globalVal.tts) {
+      ttsEngine(`Voice Speed and pitch reset to 1`);
+    }
   });
 
   $("input[name='tts']").on("change", function () {
     if ($(this).is(":checked")) {
+      globalVal.tts = true;
       ttsEngine(
         "Text to Speech has been enabled. Hover over the elements of the website to hear their content. For active inputs, press control, alt and s to read."
       );
       document.addEventListener("mouseover", handleMouseEnter);
     } else {
+      globalVal.tts = false;
       ttsEngine("Text to Speech has been disabled");
       document.removeEventListener("mouseover", handleMouseEnter);
     }
+    sessionStorage.setItem("globalVal", JSON.stringify(globalVal));
   });
 });
--- wp-content/plugins/web-accessibility-plugin/includes/class-wp-accessibility-init.php
+++ wp-content/plugins/web-accessibility-plugin/includes/class-wp-accessibility-init.php
@@ -5,29 +5,35 @@ class WPAccessibilityInit
     //Function to run on class call with the parameters that are passed
     public function __construct()
     {
-        add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
+        add_action('wp_enqueue_scripts', [$this, 'enqueue']);
+        add_action('admin_enqueue_scripts', [$this, 'enqueue']);
         add_action('wp_body_open', array($this, 'run'));
+        add_action('admin_footer', array($this, 'run'));
     }
 
     //Function for run the front-end
     public function run()
     {
-        echo '<div class="accessibility-controls">
+        $adminClass = '';
+        if (is_admin()) {
+            $adminClass = 'admin-style';
+        }
+        echo '<div class="accessibility-controls ' . $adminClass . '">
                 <div id="accessibility-menu" class="accessibility-menu">
                     <div class="font-size-control">
-                        <div><strong>Change Relative Font Size</strong></div>
+                        <div class="head"><strong>Change Relative Font Size</strong></div>
                         <div class="control">
-                            <button class="minus">
+                            <button class="minus" aria-label="decrease-font-size">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#007acc" d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
                             </button>
                             <span> 0 </span>
-                            <button class="plus">
+                            <button class="plus" aria-label="increase-font-size">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#007acc" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
                             </button>
                         </div>
                     </div>
                     <div class="color-control">
-                        <div><strong>Change Color Mode and Contrast</strong></div>
+                        <div class="head"><strong>Change Color Mode and Contrast</strong></div>
                         <div class="options">
                             <div id="color-blind-wrapper">
                                 <span>Color Mode</span>
@@ -88,7 +94,7 @@ class WPAccessibilityInit
                                 <input type="range" id="sepia" name="color-mode" value="0"/>
                             </div>-->
                             <div class="contrast-control">
-                                <span>Constrast</span>
+                                <span>Contrast</span>
                                 <input type="range" id="contrast" name="color-mode" value="0"/>
                             </div>
                             <button class="reset">
@@ -98,7 +104,7 @@ class WPAccessibilityInit
                         </div>
                     </div>
                     <div class="text-to-speech-control">
-                        <div class="tts-toggle">
+                        <div class="tts-toggle head">
                             <div><strong>Enable TTS</strong></div>
                             <input type="checkbox" name="tts" id="tts"/>
                         </div>
@@ -123,14 +129,14 @@ class WPAccessibilityInit
                         </div>
                     </div>
                 </div>
-                <button id="accessibility-button" class="accessibility-button">
+                <button id="accessibility-button" class="accessibility-button" aria-label="accessibility-button">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm161.5-86.1c-12.2-5.2-26.3 .4-31.5 12.6s.4 26.3 12.6 31.5l11.9 5.1c17.3 7.4 35.2 12.9 53.6 16.3l0 50.1c0 4.3-.7 8.6-2.1 12.6l-28.7 86.1c-4.2 12.6 2.6 26.2 15.2 30.4s26.2-2.6 30.4-15.2l24.4-73.2c1.3-3.8 4.8-6.4 8.8-6.4s7.6 2.6 8.8 6.4l24.4 73.2c4.2 12.6 17.8 19.4 30.4 15.2s19.4-17.8 15.2-30.4l-28.7-86.1c-1.4-4.1-2.1-8.3-2.1-12.6l0-50.1c18.4-3.5 36.3-8.9 53.6-16.3l11.9-5.1c12.2-5.2 17.8-19.3 12.6-31.5s-19.3-17.8-31.5-12.6L338.7 175c-26.1 11.2-54.2 17-82.7 17s-56.5-5.8-82.7-17l-11.9-5.1zM256 160a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/></svg>
                 </button>
             </div>';
     }
 
     // Enqueue CSS and JavaScript
-    public function enqueue_scripts()
+    public function enqueue()
     {
         wp_enqueue_style('accessibility-style', PLUGIN_URL . '/assets/css/style.css');
         wp_enqueue_style('select2-style', PLUGIN_URL . '/assets/css/select2.min.css');
